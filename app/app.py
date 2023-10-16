ecce8c1cbc2e035c188b28f389aaf4f4cde04a37
Seth Jones <joneseth35@gmail.com>
2023-10-16 16:35:17 -0400

    Added the create_account,login, and home.html and corresponding css

--- app/app.py
+++ app/app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Replace with a secure secret key
+
+# Dummy user data (in a real app, you'd use a database)
+users = {'user1': 'password1', 'user2': 'password2'}
 
 @app.route('/')
 def home():
-    return 'NoteMode Web App!'
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+@app.route('/create_account', methods=['GET', 'POST'])
+def create_account():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # In a real app, you'd hash the password and store it securely in a database
+        users[username] = password
+
+        session['username'] = username
+        return redirect(url_for('home'))
+
+    return render_template('create_account.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
