cc46e64e315185b902753450a4cababf1a52e6e8
Seth Jones <joneseth35@gmail.com>
2023-10-22 20:44:24 -0400

    Working on UT-3, users can now change the color and font of the mode and it will update all mode pages.

--- app/views/note_view.py
+++ app/views/note_view.py
@@ -11,10 +11,35 @@ def show_notes(mode_id):
         username = session['username']
         user_id = get_user_id(username)
 
-        notes = get_notes_by_mode(mode_id)
-        return render_template('notes.html', notes=notes, mode_id=mode_id)
+        info = get_mode_info(mode_id)
+        color = get_background_color(mode_id)
+        font = get_font(mode_id)
+        return render_template('notes.html', color=color, font=font, mode_id=mode_id)
 
     return redirect(url_for('login'))
+
+
+def get_mode_info(mode_id):
+    with sqlite3.connect('modes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM modes WHERE id = ?', (mode_id,))
+        mode_info = cursor.fetchone()
+    return mode_info
+
+def get_background_color(mode_id):
+    with sqlite3.connect('modes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT background_color FROM modes WHERE id = ?', (mode_id,))
+        background_color = cursor.fetchone()
+    return background_color[0] if background_color else None
+
+def get_font(mode_id):
+    with sqlite3.connect('modes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT font_family FROM modes WHERE id = ?', (mode_id,))
+        font = cursor.fetchone()
+    return font[0] if font else None
+
 
 # Helper function to get notes by mode ID
 def get_notes_by_mode(mode_id):
